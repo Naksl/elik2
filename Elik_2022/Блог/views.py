57036bec92704b531dd6f9a8f95091bad720987e
Naksl <113337235+Naksl@users.noreply.github.com>
2022-09-21 16:18:24 +0300

    Update and rename Elik_2022/blog/views.py to Elik_2022/Блог/views.py

--- Elik_2022/Блог/views.py
+++ Elik_2022/Блог/views.py
@@ -3,7 +3,7 @@ from .models import Post
 # Create your views here.
 
 def blog_index(request):
-    blogs = Post.objects.all().orderby('-created_on')
+    blogs = Post.objects.all().order_by('-created_on')
     context = {
         'blogs': blogs
     }
